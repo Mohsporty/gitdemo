dd473348d3fa457f3367abe3c42efcfa028703f7
Mohsporty <m.alassdi@gmail.com>
2024-01-18 17:39:57 +0300

    Initial Commit

---
+++ gitdemo/gitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemoApp.swift
+//  gitdemo
+//
+//  Created by Mohammad  on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct gitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
